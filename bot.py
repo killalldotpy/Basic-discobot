f33fbad64405d6c19692d69606cf4d4cfc4c1d0f
𝓷𝓻𝓰 <143973863+killalldotpy@users.noreply.github.com>
2024-10-19 13:30:24 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 import discord
 from discord.ext import commands
@@ -10,16 +9,13 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
     print(f'Inloggad som {bot.user}')
     
-    # Sätt status och aktivitet
+    # Set status and message
     await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.listening, name="The Code"))
     
-    # Här ersätter du 'KANAL_ID' med det riktiga ID för den kanal du vill skicka meddelandet till
-    #channel = bot.get_channel(1143989469410770975)
-    #if channel:
-        #await channel.send('DrMössa!')
+
         
 @bot.command()
 async def type(ctx, *, text):
     await ctx.send(text)
         
-bot.run('MTE0NTY3MjIwNDAzODcwMTA5OA.GgP1yz.-Y8RokC0g4MfACUHvdywPonV-YcmDURDy9Q2Zk')
+bot.run('TOKEN')
